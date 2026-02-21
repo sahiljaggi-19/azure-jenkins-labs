8cd0f262d2f9733239847b746120f97952037271
Ubuntu <vinod@master.ypdvigp3m2ouxe04p1vqhcf3xd.gx.internal.cloudapp.net>
2026-02-21 03:01:02 +0000

    Fixed apt typo in Jenkins install script

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -11,14 +11,13 @@ echo"confirming version"
 
 java -version
 
-curl -fsSl
 
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
 
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-sudo app update
-sudo app install jenkins
+sudo apt update
+sudo apt install jenkins
 
 sudo systemctl enable jenkins
 
