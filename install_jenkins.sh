c09e6ff0759e0ca704a25f0660e65f83e838f61b
Vinod Kumar <55591882+sahiljaggi-19@users.noreply.github.com>
2026-03-03 11:38:43 +0530

    Add -y flag to jenkins installation command

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -17,7 +17,7 @@ sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 sudo apt update
-sudo apt install jenkins
+sudo apt install jenkins -y
 
 sudo systemctl enable jenkins
 
