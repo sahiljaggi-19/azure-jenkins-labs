fb83a1beebe5de3a5574555d9973f2e9b6cd7eb9
Vinod Kumar <55591882+sahiljaggi-19@users.noreply.github.com>
2026-03-04 14:21:40 +0530

    installing dockers
    Add commands to start Docker and run hello-world container.

---
+++ install_docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+  sudo apt-get update
+
+sudo systemctl enable docker
+sudo systemctl start docker
+
+newgrp docker
+
+docker run hello-world
